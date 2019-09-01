ca99513d92e90a7c5b11357d57e12ee252094b8d
anderson <andnunes88@gmail.com>
2019-09-01 19:16:12 -0300

    add paginas de erro

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+
+	<title>404 HTML Template by Colorlib</title>
+
+	<!-- Google font -->
+	<link href="https://fonts.googleapis.com/css?family=Kanit:200" rel="stylesheet">
+
+	<!-- Font Awesome Icon -->
+	<link type="text/css" rel="stylesheet" href="{{asset('assets/site/css/font-awesome.min.css')}}" />
+
+	<!-- Custom stlylesheet -->
+	<link type="text/css" rel="stylesheet" href="{{asset('assets/site/css/style.css')}}" />
+
+	
+
+</head>
+
+<body>
+
+	<div id="notfound">
+		<div class="notfound">
+			<div class="notfound-404">
+				<h1>4<img src="{{asset('images/icons/icon-128x128.png')}}">4</h1>
+			</div>
+			<h2>Oops! Página não encontrada</h2>
+			<p>A página que você está procurando não existe ou foi removida. <a href="{{route('site.todos.produtos')}}">Voltar para site</a></p>
+			<div class="notfound-social">
+				<a href="https://www.facebook.com/autodicasbr"><i class="fa fa-facebook"></i></a>
+				<a href="#"><i class="fa fa-twitter"></i></a>
+				
+			</div>
+		</div>
+	</div>
+
+</body><!-- This templates was made by Colorlib (https://colorlib.com) -->
+
+</html>
